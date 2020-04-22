f9e19cfaf54ba01c95e49497627ac2ca3ff2a240
Marko Milić <marko@tesobe.com>
2020-04-22 13:16:04 +0200

    add use_custom_webapp props

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -46,6 +46,7 @@ import java.io.File
 import code.lib.OAuthClient
 import code.util.{Helper, MyExceptionLogger}
 import code.util.Helper.MdcLoggable
+import org.apache.commons.io.FileUtils
 
 /**
  * A class that's instantiated early and run.  It allows the application
@@ -194,6 +195,31 @@ class Boot extends MdcLoggable{
 
     LiftRules.uriNotFound.prepend(NamedPF("404handler"){ case (req,failure) =>
       NotFoundAsTemplate(ParsePath(List("404"),"html",true,false)) })
+
+    //If use_custom_webapp=true, this will copy all the files from `OBP-API/obp-api/src/main/webapp` to `OBP-API/obp-api/src/main/resources/custom_webapp`
+    if (Helper.getPropsAsBoolValue("use_custom_webapp", false)){
+      //this `LiftRules.getResource` will get the path of `OBP-API/obp-api/src/main/webapp`: 
+      LiftRules.getResource("/").map { url =>
+        // this following will get the path of `OBP-API/obp-api/src/main/resources/custom_webapp`
+        val source = if (getClass().getClassLoader().getResource("custom_webapp") == null)
+          throw new RuntimeException("If you set `use_custom_webapp = true`, custom_webapp folder can not be Empty!!")
+        else
+          getClass().getClassLoader().getResource("custom_webapp").getPath
+        val srcDir = new File(source);
+
+        // The destination directory to copy to. This directory
+        // doesn't exists and will be created during the copy
+        // directory process.
+        val destDir = new File(url.getPath)
+
+        // Copy source directory into destination directory
+        // including its child directories and files. When
+        // the destination directory is not exists it will
+        // be created. This copy process also preserve the
+        // date information of the file.
+        FileUtils.copyDirectory(srcDir, destDir)
+      }
+    }
 
   }
 }
